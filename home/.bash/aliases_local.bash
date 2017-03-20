e58bd4cc6aa7a7702ddc689b30117ea1931b5050
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-03-20 01:56:22 +0100

    added ubuntu config

---
+++ home/.bash/aliases_local.bash
@@ -0,0 +1,7 @@
+# shortcuts for cd
+code=~/Documents/Code
+ds=~/Documents/Code/DataScience
+lit=~/Documents/Lit
+ref=~/Documents/Lit/refs
+mounts=/var/run/user/1000/gvfs
+
---
+++ home/.bash/path_local.bash
@@ -0,0 +1,6 @@
+# set PATH so it includes user's private bin directories
+PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+
+# Anaconda
+path_prepend $HOME/anaconda/bin
+
