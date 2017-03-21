5235251768a42d9d6fe38645d1a13bf96b680eb1
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-03-21 17:25:03 +0100

    PY: added automatic imports of numpy to jupyter

--- home/.ipython/profile_default/ipython_kernel_config.py
+++ home/.ipython/profile_default/ipython_kernel_config.py
@@ -1,2 +1,2 @@
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['import sys', 'import os', 'sys.path.append(os.path.join(os.path.expanduser("~"),"Documents/Code/DataScience/"))', '%autoreload 2']
+c.InteractiveShellApp.exec_lines = ['import sys', 'import os', 'sys.path.append(os.path.join(os.path.expanduser("~"),"Documents/Code/DataScience/"))', 'import numpy as np', '%autoreload 2']
