29a8fea138557f0f5d70f32c501964b680e2ecf0
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-03-21 14:35:45 +0100

    PY: added commands to be executed on IPython kernel start-up

---
+++ home/.ipython/profile_default/ipython_kernel_config.py
@@ -0,0 +1,2 @@
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = ['import sys', 'import os', 'sys.path.append(os.path.join(os.path.expanduser("~"),"Documents/Code/DataScience/"))', '%autoreload 2']
