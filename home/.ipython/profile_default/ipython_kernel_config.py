c1788b7775cf35c6ebfd7262e2a1c06618e1f0b1
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-03-22 18:27:27 +0100

    PY: start matplotlib with qt5Agg by default

--- home/.ipython/profile_default/ipython_kernel_config.py
+++ home/.ipython/profile_default/ipython_kernel_config.py
@@ -1,2 +1,3 @@
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['import sys', 'import os', 'sys.path.append(os.path.join(os.path.expanduser("~"),"Documents/Code/DataScience/"))', 'import numpy as np', '%autoreload 2']
+c.InteractiveShellApp.matplotlib = 'qt5'
