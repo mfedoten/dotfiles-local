69c1229fa76b6ebbda5890089e450a45689b00e5
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-03-22 18:28:50 +0100

    PY: start matplotlib with OSX by default

--- home/.ipython/profile_default/ipython_kernel_config.py
+++ home/.ipython/profile_default/ipython_kernel_config.py
@@ -1,2 +1,3 @@
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['import sys', 'import os', 'import numpy as np', '%autoreload 2']
+c.InteractiveShellApp.matplotlib = 'osx'
