f666e8c4c8e283ba33570977feacf6a8996923c4
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-03-28 18:51:06 +0200

    PY: using PYTHONPATH before I finally start using virtual env

--- home/.bash/01_path_local.bash
+++ home/.bash/01_path_local.bash
@@ -1,6 +1,9 @@
 # set PATH so it includes user's private bin directories
-PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+path_prepend $HOME/bin
+path_prepend $HOME/.local/bin
 
 # Anaconda
 path_prepend $HOME/anaconda/bin
 
+# Add PYTHONPATH before you actually start using virtual env
+PYTHONPATH=/home/mariia/Code/DataScience:$PYTHONPATH
