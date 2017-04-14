44d5167bd7dffcaf61175754ded51bd0389736ce
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-04-14 13:45:02 +0200

    BASH: added path to sonarLing

--- home/.bash/01_path_local.bash
+++ home/.bash/01_path_local.bash
@@ -5,5 +5,8 @@ path_prepend $HOME/.local/bin
 # Anaconda
 path_prepend $HOME/anaconda/bin
 
+# SonarLint
+path_append /opt/sonarlint-cli-2.1.0.566/bin
+
 # Add PYTHONPATH before you actually start using virtual env
 export PYTHONPATH=/home/mariia/Documents/Code/DataScience:$PYTHONPATH
