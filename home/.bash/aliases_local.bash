179f63feb6190bbe2b89b0fdef1f33b7173607be
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-05-12 17:47:37 +0200

    changed some aliases

--- home/.bash/aliases_local.bash
+++ home/.bash/aliases_local.bash
@@ -1,8 +1,8 @@
 # shortcuts for cd
 pr=~/Documents/Code
 ds=~/Documents/Code/DataScience
-lit=~/Documents/Lit
-ref=~/Documents/Lit/refs
+lit=/mnt/hgfs/Lit
+ref=/mnt/hgfs/Lit/refs
 mounts=/var/run/user/1000/gvfs
 ser_data=/media/serena/data
 ser_tech=/media/serena/tech
