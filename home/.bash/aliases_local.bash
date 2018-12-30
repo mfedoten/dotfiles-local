a67045c6c3022d48f45c8c5dac16547f976e0cb8
mfedoten <maria.fedotenkova@gmail.com>
2018-12-30 18:04:19 +0100

    os-specific aliases

--- home/.bash/aliases_local.bash
+++ home/.bash/aliases_local.bash
@@ -1,3 +1,13 @@
+alias pbc='xclip -selection clipboard'
+alias pbp='xclip -selection clipboard -o'
+alias o='xdg-open'
+alias oo='nautilus .'
+alias on='nautilus'
+alias ge="gedit"
+
+# Update APT, installed packages and locateDB
+alias update='sudo apt-get -qq update; sudo apt-get -qq dist-upgrade; sudo apt autoremove; sudo apt-get autoclean; sudo updatedb'
+
 # shortcuts for cd
 pr=~/Documents/Code
 ds=~/Documents/Code/DataScience
