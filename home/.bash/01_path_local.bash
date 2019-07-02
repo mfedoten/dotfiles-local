a90c2cd41591a155b8b2761057ab7d2442d8d506
mariia <mariia@dreem.com>
2019-07-02 19:50:14 +0200

    rolled back to anaconda

--- home/.bash/01_path_local.bash
+++ home/.bash/01_path_local.bash
@@ -2,13 +2,17 @@
 path_prepend $HOME/bin
 path_prepend $HOME/.local/bin
 
-# Anaconda stuff
-# if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
-#     . $HOME/miniconda3/etc/profile.d/conda.sh
-#     conda activate
-# fi
+Anaconda stuff
+if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+    . $HOME/miniconda3/etc/profile.d/conda.sh
+    conda activate
+fi
 
-path_prepend $HOME/anaconda/bin
+# path_prepend $HOME/miniconda3/bin
+# # Virtualenv wrapper stuff
+# export WORKON_HOME=$HOME/code/.virtualenvs
+# mkdir -p $WORKON_HOME
+# source $HOME/miniconda3/bin/virtualenvwrapper.sh 2>/dev/null
 
 # Add PYTHONPATH before you actually start using virtual env
 # pydir=/home/mariia/Documents/Code/DataScience
